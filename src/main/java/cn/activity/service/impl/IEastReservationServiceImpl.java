50c694613f0d8367f578c4e10748cf1491b84be5
qinyongbo <qinyongbo@chudaokeji.com>
2017-06-05 16:07:42 +0800

    init project

---
+++ src/main/java/cn/activity/service/impl/IEastReservationServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.activity.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.activity.bean.Person;
+import cn.activity.service.IEastReservationService;
+@Service("eastReservationService")
+@SuppressWarnings(value="all")
+public class IEastReservationServiceImpl implements IEastReservationService {
+
+	@Override
+	public int getPerson(Person person) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
